5d1d8c202a042d23571f06a984cae23231e0eeaa
PAWAN GOEL -X (pawgoel - HCL TECHNOLOGIES LIMITED at Cisco) <pawgoel@cisco.com>
2023-12-17 01:43:22 +0530

    Main Branch

---
+++ emp_app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Employee 
+# Role, Department, Leave, Attendance, Manager
+# Register your models here.
+
+admin.site.register(Employee)
+# admin.site.register(Role)
+# admin.site.register(Department)
+# admin.site.register(Leave)
+# admin.site.register(Attendance)
+# admin.site.register(Manager)
---
+++ emp_app/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+
+# Create your models here.
+
+
+# class Department(models.Model):
+#     name = models.CharField(max_length=100,null=False)
+#     location = models.CharField(max_length=100)
+
+#     def __str__(self):
+#         return self.name
+
+
+# class Role(models.Model):
+#     name = models.CharField(max_length=100, null=False)
+
+#     def __str__(self):
+#         return self.name
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100, null=False)
+    last_name = models.CharField(max_length=100)
+    # cisco_id = models.CharField(max_length=100)
+    status = models.CharField(max_length=1000, default='0000000', null=True)
+    # dept = models.ForeignKey(Department, on_delete=models.CASCADE)
+    # salary = models.IntegerField(default=0)
+    # bonus = models.IntegerField(default=0)
+    # role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    # phone = models.IntegerField(default=0)
+    # hire_date = models.DateField()
+
+
+    def __str__(self):
+        return "%s %s %s" %(self.first_name, self.last_name, self.status)
+        # return "%s %s %s" %(self.first_name, self.last_name, self.phone)
+
+
+# class Leave(models.Model):
+#     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+#     leave_type = models.CharField(max_length=100)
+#     start_date = models.DateField()
+#     end_date = models.DateField()
+#     reason = models.CharField(max_length=100)
+
+#     def __str__(self):
+#         return self.leave_type
+
+# class Attendance(models.Model):
+#     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+#     date = models.DateField()
+#     status = models.CharField(max_length=100)
+
+#     def __str__(self):
+#         return self.status
+
+# class Manager(models.Model):
+#     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+#     dept = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.employee.first_name
+        
