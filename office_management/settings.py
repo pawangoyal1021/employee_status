1af290c36ae28d124485dc64a141e65d4b642d51
PAWAN GOEL -X (pawgoel - HCL TECHNOLOGIES LIMITED at Cisco) <pawgoel@cisco.com>
2023-12-18 01:37:27 +0530

    hiruko

--- office_management/settings.py
+++ office_management/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-04n$_ed7imuc#a1)407lut0-rs%@i6#a(dbo--ihp2r4_lro$5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'office_management.urls'
